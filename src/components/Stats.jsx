f3f5eb0d29a278732c523840e485cfd7147f9cb4
Igor Babin <babinigor.3@yandex.ru>
2024-09-03 17:39:26 +0300

    1.0.1

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,10 +1,21 @@
+import {
+  FACEBOOK_MAX_CHARACTERS,
+  INSTAGRAM_MAX_CHARACTERS,
+} from "../lib/constants";
+
 export default function Stats({ numberOfCharacters, numberofWords }) {
   return (
     <section className="stats">
       <Stat number={numberofWords} label="Words" />
       <Stat number={numberOfCharacters} label="Characters" />
-      <Stat number={280 - numberOfCharacters} label="Instagram" />
-      <Stat number={2200 - numberOfCharacters} label="Facebook" />
+      <Stat
+        number={INSTAGRAM_MAX_CHARACTERS - numberOfCharacters}
+        label="Instagram"
+      />
+      <Stat
+        number={FACEBOOK_MAX_CHARACTERS - numberOfCharacters}
+        label="Facebook"
+      />
     </section>
   );
 }
